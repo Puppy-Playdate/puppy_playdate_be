45c16e3b457b5a70853829dba7bd753dd473b88e
Tommy Takahashi <ttakahashi1591@gmail.com>
2024-01-06 13:46:22 -0700

    test: Add Model testing for User and Dog

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should allow_value('user@example.com').for(:email) }
+    it { should_not allow_value('invalid_email').for(:email) }
+    it { should validate_presence_of(:password_digest) }
+  end
+
+  describe 'associations' do
+    it { should have_many(:dogs) }
+  end
+
+  describe 'has_secure_password' do
+    it { should have_secure_password }
+  end
+end
